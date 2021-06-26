f4c6842ab8e8d13e3b741394896ddbc270c4626f
vagabond44 <86507912+vagabond44@users.noreply.github.com>
2021-06-26 13:58:28 +0800

    initial commit

---
+++ js/data.js
@@ -0,0 +1,16 @@
+
+var fiefQuests = [
+    ["Fehelovak", "5", "Long Cotton", "43000"],
+    ["Hatvan", "5", "Haematite", "14000"],
+    ["Vashegy", "4", "Ash Timber", "44000"],
+    ["Tabori Var", "5", "Chalcopyrite", "22000"],
+    ["Tabori Var", "5", "Ash Timber", "57000"]
+];
+
+// x,y coordinate of the fiefs at the center
+var coordinateMap = {
+    "Fehelovak": { "x": 479, "y": 75 },
+    "Hatvan": { "x": 712, "y": 331 },
+    "Vashegy": { "x": 298, "y": 263 },
+    "Tabori Var": { "x": 200, "y": 340 }
+}
